3e1686b4b012e5a5a5be8721bbb7bac5197ae7aa
Jederson Sousa Luz <jedersonalpha@gmail.com>
2023-03-11 10:52:45 -0300

    Update views.py
    Criação das views que irão gerar as rotas da API REST

--- backend/server/api/views.py
+++ backend/server/api/views.py
@@ -1,3 +1,87 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from server.api.serializers import (
+    UserSerializer, 
+    GroupSerializer,
+    CustomerSerializer,
+    FinancialInstitutionSerializer,
+    AuctionSerializer,
+    RealEstateSerializer,
+    VehicleSerializer,
+    BidHistorySerializer
+)
+from .models import (
+    Customer,
+    FinancialInstitution,
+    Auction,
+    RealEstate,
+    Vehicle,
+    BidHistory
+)
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class FinancialInstitutionViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = FinancialInstitution.objects.all()
+    serializer_class = FinancialInstitutionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class AuctionViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Auction.objects.all()
+    serializer_class = AuctionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class RealEstateViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = RealEstate.objects.all()
+    serializer_class = RealEstateSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class VehicleViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Vehicle.objects.all()
+    serializer_class = VehicleSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class BidHistoryViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = BidHistory.objects.all()
+    serializer_class = BidHistorySerializer
+    permission_classes = [permissions.IsAuthenticated]
