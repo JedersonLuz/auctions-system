22709c63203a8601124affe7a3fe1a6607611fc0
JedersonLuz <jedersonalpha@gmail.com>
2023-03-16 20:11:55 -0300

    Added JWT Authentication to the backend

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -143,6 +143,9 @@ STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
